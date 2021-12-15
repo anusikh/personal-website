463100cd06b6be3620c187ffaa5c7f985a33134e
anusikh <anusikh2001@gmail.com>
2021-12-16 00:06:04 +0530

    Added Projects Page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,31 @@
 import React from "react";
+import { FaGithub, FaGithubAlt } from "react-icons/fa";
+import "./styles/Projects.css";
+import x from "./utils/projects.json";
 
 function Projects() {
   return (
-    <div>
-      <p>Projects</p>
+    <div className="Project__Main">
+      {Object.keys(x).map((i) => {
+        return (
+          <div className="Project__Container" key={i}>
+            <div className="Project__Header">{x[i].heading}</div>
+            <img
+              className={x[i].id === 3 ? "Project__Gif" : "Project__Image"}
+              src={x[i].imageUrl}
+              alt="Loading"
+            />
+            <div className="Project__Description">{x[i].description}</div>
+            <button
+              className="Project__Url"
+              onClick={() => window.open(x[i].url, "_blank")}
+            >
+              <FaGithubAlt size={20} />
+              &nbsp; Github
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 }
