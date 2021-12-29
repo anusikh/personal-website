bd1b9c400b3603ffd2165954dbfbf85edb4094c1
anusikh <anusikh2001@gmail.com>
2021-12-29 15:44:17 +0530

    Added Auth Logic

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import Home from "./components/Home";
 import Blog from "./components/Blog";
 import Projects from "./components/Projects";
 import Editor from "./components/Editor";
+import Authenticate from "./components/Authenticate";
+import { dataContext } from "./components/context/dataContext";
+import React from "react";
 
 function App() {
+  // Context Api
+  const dataCon = React.useContext(dataContext);
+  const { authenticated } = dataCon;
+
   return (
     <BrowserRouter>
       <Routes>
@@ -13,9 +20,10 @@ function App() {
           <Route path="blog/:id" element={<Blog />} />
           <Route path="projects" element={<Projects />} />
           <Route
-            path={`${process.env.REACT_APP_SECRET_PATH}`}
-            element={<Editor />}
+            path="editor"
+            element={authenticated ? <Editor /> : <>Not Authenticated</>}
           />
+          <Route path="authenticate" element={<Authenticate />} />
         </Route>
         <Route path="*" element={<p>Sorry, nothing here</p>} />
       </Routes>
